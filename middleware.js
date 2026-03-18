f13e737252c985bf3dd977fd2d788a496f58713d
Danai Harawa <danaiharawa@icloud.com>
2026-03-18 17:57:30 +1100

    remove password protection

--- middleware.js
+++ middleware.js
@@ -1,20 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const cookie = request.cookies.get('yi-access')
-  
-  // Allow access to the password page itself
-  if (request.nextUrl.pathname === '/enter') {
-    return NextResponse.next()
-  }
-
-  // If they have the cookie, let them through
-  if (cookie?.value === 'granted') {
-    return NextResponse.next()
-  }
-
-  // Otherwise redirect to password page
-  return NextResponse.redirect(new URL('/enter', request.url))
+  return NextResponse.next()
 }
 
 export const config = {
